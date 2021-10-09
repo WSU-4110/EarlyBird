72f46e1d5c713250fdd865f8f1a99cbb1e964c77
Hegga Nassr <gv9159@wayne.edu>
2021-10-09 09:10:49 -0400

    Navigation bar, Router issues

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import './Navbar.css';
+import Register from "./Register";
+
+
 
 
 function Navbar() {
 
     //return Navbar here
-    return <nav className="navbar bg-dark container">
+    return <nav class="navbar">
+
+
+<ul class="tabs">
+
+<a href = "./Home"><img src = "images/logo.png" alt = "logo" class = "logo"></img></a>
+   
+    
+     <li class="item-right">
+        <Link class="Login" to = "/Login">Login</Link>
+     </li>
+     <p class = "item-right">/</p>
+     <li class="item-right">
+        <Link class="CreateAccount" to = "/Register">Create Account</Link>
+        
+     </li>
+
+
+</ul>
+
 
     </nav>
 
