3cf5a4ed44ee517e8c9e128cbcd0c14c35785028
Hegga Nassr <gv9159@wayne.edu>
2021-10-10 11:38:40 -0400

    Login/Register functioning

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import './Register.css';
+import { render } from "react-dom";
+import { GoogleLogin } from "react-google-login";
+
+
+const responseGoogle= response => {
+    console.log(response);
+
+  }
+
+
 
 function Register() {
 
-    return <div>
-        <h1>Create Register page</h1>
+    
+    return(
+    <div class = "signIn">
+        
+    <GoogleLogin clientId = "874934368638-v7dtscnojaffh40c091bd0msjl0j5idl.apps.googleusercontent.com" 
+                button ="Sign in" onSuccess ={responseGoogle}
+            onFailure={responseGoogle} />
+
+
+
     </div>
+    );
 }
+    
+    
+
 
 export default Register;
+    
