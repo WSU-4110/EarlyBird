de3851898d258ccc10681493a4ebdfc068e1b4ea
Bella-Su <bellabailey18@outlook.com>
2021-10-12 17:25:52 -0400

    Ojaswee created TechInterview.css and worked with layout of the techInterviw page

--- frontend/src/components/TechInterview.jsx
+++ frontend/src/components/TechInterview.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import './TechInterview.css';
 
 function TechInterview() {
 
     return <div>
-        <h1>Create Technical Interview page</h1>
+        <article>
+            <h1>Technical Interview</h1>
+            <p>Here you will be able to find tips and tricks to help you prepare with your Technical Interview.</p>
+        </article>
+
     </div>
 }
 
