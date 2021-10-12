7220aad06c812d84210a8363dd0cb577f1212cc6
Bella-Su <bellabailey18@outlook.com>
2021-10-12 10:55:28 -0400

    applied style on CreatePost

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ReactHtmlParser, {precessNodes, convertNodeToElement, htmlparser2} from 'react-html-parser';
+import './CreatePost.css'
 
 function CreatePost() {
-    const [text, setText] = useState("")
+    const [text, setText] = useState("");
 
     return (
-        <div className='container-md'>
+        <div className='createArea container-md'>
             <h4>Create a post</h4>
             <form className="row g-3">
                 <div className="form-group col-md-6">
-                    <select class="form-select">
+                    <select className="selections form-select">
                         <option selected>Choose a post type</option>
                         <option>Project</option>
                         <option>Technical Interview</option>
@@ -18,22 +20,24 @@ function CreatePost() {
                     </select>
                 </div>
                 <div className="form-group">
-                    <input className='form-control' placeholder="Title"></input>
+                    <input className='title form-control' placeholder="Title" name="title"></input>
                 </div>
 
                 <div className="form-group editor">
                     <CKEditor
+                        className="form-control"
                         editor={ClassicEditor}
                         data={text}
                         onChange={(event, editor) => {
                             const data = editor.getData()
-                            setText(data)
+                            setText(ReactHtmlParser(data))
                         }}
                     />
                 </div>
 
-                <div class="col-auto">
-                    <button type="submit" class="btn btn-secondary">POST</button>
+                <div class="button col-auto">
+                <button type="button" className="cancelBtn btn-light">CANCEL</button>
+                <button type="submit" className="submitBtn btn btn-secondary">POST</button>  
                 </div>
             </form>
         </div>)
