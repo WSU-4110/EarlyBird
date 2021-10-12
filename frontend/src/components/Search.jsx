2ef5b80743c1ed8b66c73f570ddfae75d1ffaf03
Mazen Zeidan <go5334@wayne.edu>
2021-10-11 21:11:00 -0400

    Search Bar

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import './Search.css';
 
 function Search() {
-
+ 
     return <div>
-        <h1>Create Search funtion</h1>
+      
+        <nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
     </div>
 }
-
+ 
 export default Search;
