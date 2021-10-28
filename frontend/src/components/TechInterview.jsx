7e00f7c1c4a0c12288819f0e59152384eac1b52e
OjasweeP <91394698+OjasweeP@users.noreply.github.com>
2021-10-28 15:43:17 -0400

    Technical Interview

--- frontend/src/components/TechInterview.jsx
+++ frontend/src/components/TechInterview.jsx
@@ -6,10 +6,14 @@ function TechInterview() {
     return <div>
         <article>
             <h1>Technical Interview</h1>
-            <p>Here you will be able to find tips and tricks to help you prepare with your Technical Interview.</p>
+            <p> Here you will be able to find tips and tricks to help you prepare with your Technical Interview.</p>
         </article>
 
     </div>
+}
+<div>className "TechPost"</div>
+{
+
 }
 
 export default TechInterview;
