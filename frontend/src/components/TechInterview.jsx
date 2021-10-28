448de04e06b91ffdda69e7a9e5729867a98845b8
OjasweeP <91394698+OjasweeP@users.noreply.github.com>
2021-10-28 19:57:24 -0400

    Tech Interview

---
+++ frontend/src/components/TechInterview.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './TechInterview.css';
+function TechInterview(){
+    return <div>
+        <div class = "info">
+            <h1>Technical Interview</h1>
+            <p>Here you will be able to find tips and tricks to help you prepare with youe Technical Interview</p>
+        </div>
+    </div>
+}
+export default TechInterview;
