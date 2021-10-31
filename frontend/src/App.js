b126bbb3172852a19dcf77590d19581c6bb9226d
Hegga Nassr <gv9159@wayne.edu>
2021-10-31 19:49:11 -0400

    Render input to Projects page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import CreatePost from './components/CreatePost';
 import Home from './components/Home';
@@ -9,20 +9,14 @@ import StudyRes from './components/StudyRes';
 import Register from './components/Register';
 import Login from './components/Login';
 import Search from './components/Search';
+import Results from './components/Results';
+import Post from './components/Post';
 
 
 
 
 function App() {
 
-
-  //we will need to return:
-  //Navbar '
-  //Home page
-  //Projects page
-  //Technical Interview page
-  //Study Rescource page
-  //About page
   return (
     
     
@@ -46,6 +40,12 @@ function App() {
           </Route>
           <Route path= "/CreatePost">
            <CreatePost/>
+          </Route>
+          <Route path= "/Results">
+           <Results/>
+          </Route>
+          <Route path= "/Post">
+           <Post/>
           </Route>
           <Route path= "/Login">
            <Login/>
