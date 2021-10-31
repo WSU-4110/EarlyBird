968c01a618b645a47e132b476e0b425342aed0d8
Hegga Nassr <gv9159@wayne.edu>
2021-10-31 19:08:53 -0400

    Render input to Projects tab

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import './Projects.css'
+
+var category;
 
 function Projects() {
 
-    return <div>
-        <h1>Create Projects page</h1>
-    </div>
-}
+    const [projectPost, setProjectPost] = useState([{
+        category: '',
+        title: '',
+        content: ''
+    }])
+    
+    useEffect(()=> {
+        const post = async() => {
+         const response = await axios('http://localhost:3001/proj')
+         setProjectPost(response.data);
+         };
+         post();
+      });
+      
+      
+     const Posts = projectPost.map((p) => {
+          return <div>
+          <h2>{p.title}</h2>
+          <p>{p.content}</p>
+          </div>  
+      })
+    
+    
+ return <div class = "projectPosts">
+                    <h1> Projects </h1>
+                    {Posts}
+
+                    </div>
+} 
 
 export default Projects;
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,5 +1,4 @@
 const express = require("express");
-//const { model } = require("mongoose");
 const router = express.Router();
 const Post = require("../models/postModel");
 
@@ -17,6 +16,16 @@ router.route("/postCreation").post((req, res) =>{
 
     newPost.save();
 })
+
+//Render input into Projects tab
+
+    router.route("/proj").get((req,res) => {
+     
+       Post.find()
+            .then(postFound => res.json(postFound))
+          
+     
+     });
 
 
 
