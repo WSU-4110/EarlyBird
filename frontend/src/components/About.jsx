d9bae305d7d64fcfc459e7c447e24a43f82cbadf
madlyDarby <90344169+madlyDarby@users.noreply.github.com>
2021-11-01 12:10:37 -0400

    Update About.jsx
    Added About details

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -3,7 +3,33 @@ import React from "react";
 function About() {
 
     return <div>
-        <h1>Create About page</h1>
+         <center> <h1>EarlyBird</h1></center>
+
+    <article>
+      <section>
+        <p><center>
+        An online community here to support computer science students and graduates, helping them gain the confidence and resume they need to enter the workforce. <br></br>
+        <b>Our mission</b> is to bring the knowledge, support, and career building projects to you through an easy to use interface with team-members across the globe! <br></br> 
+           Your time spent on EarlyBird will build you your dream resume and prepare you for its accompanying dream job.
+          How does EarlyBird accomplish their goal? <br></br> 
+          By providing every member access to our study resources and each other!
+          From ask forums and projects to build, to study resources and technical interview help, <br></br> &nbsp; EarlyBird is here to support you through your career.
+           Go on ahead and apply for project membership ASAP.</center>
+          <br></br><br></br>
+          <em> &nbsp;&nbsp;&nbsp;Be an EarlyBird User today!</em> 
+          <br></br><br></br>
+          <ol type="1">
+            <li>Create/Login to your account with your favorite email.</li>
+            <li>Locate what you need! </li>
+            <li>Help create yours and other EarlyBird projects.</li>
+            </ol>
+          <br></br>
+          <button class="button"><a href="register.html"></a>Register</button>
+          <br></br><br></br>
+        </p>
+      </section>
+    </article>
+          
     </div>
 }
 
