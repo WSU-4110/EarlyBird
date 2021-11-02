5d27b0a7c76ce7834272020eda734dc03bb2e3b0
Mazen Zeidan <mazenzeidan@Mazens-MacBook-Pro.local>
2021-11-02 01:08:34 -0400

    Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,27 @@
 import React from "react";
-
+import './Home.css'
+ 
+//The Home function shows the three images for Projects, Study Resources, Create Posts pages. When the user clicks on the images. It will take them to those pages.
 function Home() {
-
-    return <div>
-        <h1>Home page is on the way ... </h1>
-    </div>
+   return <div>
+       <div class = "link">
+           <div class = "image">
+               <a href = "/Projects">
+               <h1>Projects</h1><img src="https://www.kindpng.com/picc/m/390-3904786_digital-marketing-agency-mission-animation-banner-hd-png.png" width ="400" height = "480" title = "Projects" alt = "Projects"></img>
+               </a>
+           </div>
+           <div class= "image">
+               <a href="/StudyRes">
+               <h1>Study Resourcs</h1><img src="https://www.pngitem.com/pimgs/m/7-78326_kisspng-cartoon-animation-clip-art-cartoon-books-5aa7d9b3c343d6.png" width ="400" height = "480" title = "Study Resources" alt = "Study Resources"></img>
+               </a>
+           </div>
+           <div class = "image">
+               <a href="./CreatePost">
+               <h1>Create Posts</h1><img src="https://images.clipartlogo.com/files/istock/previews/9436/94369965-typing-with-laptop-blank-screen-for-text-space.jpg" width ="400" height = "480" title = "Create Post" alt = "Create Posts"></img>
+               </a>
+           </div>
+       </div>
+   </div>
 }
-
+ 
 export default Home;
