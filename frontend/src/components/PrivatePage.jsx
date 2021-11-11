d60bdd7f2d7b9bcd7ee67902c3c2249f2e9c6b64
madlyDarby <90344169+madlyDarby@users.noreply.github.com>
2021-11-11 11:56:47 -0500

    Create PrivatePage.jsx
    A private page for user to logout. Can finer edits later

---
+++ frontend/src/components/PrivatePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LgOut from './LgOut';
+
+const PrivatePage = () => {
+    return (
+        <div>
+            <h2>Private Page</h2>
+            <LgOut/>
+        </div>
+    );
+};
+
+export default PrivatePage;
