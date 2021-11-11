b684bda8c05bf2c5267c6a1f7734ca1539e0dc78
madlyDarby <90344169+madlyDarby@users.noreply.github.com>
2021-11-11 12:00:38 -0500

    Create PublicPage.jsx

---
+++ frontend/src/components/PublicPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LgIn from './LgIn';
+
+const PublicPage = () => {
+    return (
+        <div>
+            <h2> Public Page </h2>
+            <LgIn/>
+        </div>
+    );
+};
+
+export default PublicPage;
