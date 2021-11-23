de96f6ba53a019424cece041c69abfa0e9b6bacf
Hegga Nassr <gv9159@wayne.edu>
2021-11-23 17:34:36 -0500

    Commit

--- frontend/src/components/TechInterview.test.jsx
+++ frontend/src/components/TechInterview.test.jsx
@@ -1,6 +1,8 @@
 import '@testing-library/jest-dom';
 import { render, screen, cleanup } from '@testing-library/react';
 import TechInterview from './TechInterview';
+import userEvent from "@testing-library/user-event";
+import React, { useEffect, useState } from "react";
 
 //Test 2
 describe('in Technical Interview tab', () => {
@@ -10,3 +12,42 @@ describe('in Technical Interview tab', () => {
         expect(screen.getByText('Technical Interviews'));
     })
     });
+
+//Test 3
+test('Ensure calling Posts' , () => {
+    render(<TechInterview/>);
+//const te = screen.getByText(/Posts/i);
+const ensureCalling =  screen.getByTestId('test3');
+//userEvent(te).toHave('title')
+
+expect(ensureCalling);
+
+});
+
+//Test 4
+const Proj = () => [projectPost, setProjectPost] = useState([{
+    
+   category: '',
+    title: '',
+    content: ''
+}])
+
+test('Ensure var take string', () =>{
+    expect(useState.category = "abcd");
+    expect(useState.title = "title");
+    expect(useState.content = "this is content");
+});
+
+//Test 5
+function filterData(inputs, key){
+    key = 'Technical Interview'
+    return inputs.filter((input) => 
+    input.category.includes(key));
+    
+  }
+    
+  test('FilterData', () =>{
+         expect(filterData.key = 'Technical Interview');
+        
+  });
+  
