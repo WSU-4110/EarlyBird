b758615b7e04d1e99d5df99a2df0f39a771651d2
Hegga Nassr <gv9159@wayne.edu>
2021-11-23 21:37:42 -0500

    {Nassr_Hegga}_unittest

--- frontend/src/components/Dropdown.jsx
+++ frontend/src/components/Dropdown.jsx
@@ -20,6 +20,7 @@ function Dropdown(){
             <Link
             className={item.cName}
             to={item.path}
+            
             onClick={() => setClick(false)}>
                 {item.title}
             </Link>
--- frontend/src/components/TechInterview.test.jsx
+++ frontend/src/components/TechInterview.test.jsx
@@ -4,39 +4,6 @@ import TechInterview from './TechInterview';
 import userEvent from "@testing-library/user-event";
 import React, { useEffect, useState } from "react";
 
-//Test 2
-describe('in Technical Interview tab', () => {
-    test('Tab?' , () => {
-        render(<TechInterview />)
-      
-        expect(screen.getByText('Technical Interviews'));
-    })
-    });
-
-//Test 3
-test('Ensure calling Posts' , () => {
-    render(<TechInterview/>);
-//const te = screen.getByText(/Posts/i);
-const ensureCalling =  screen.getByTestId('test3');
-//userEvent(te).toHave('title')
-
-expect(ensureCalling);
-
-});
-
-//Test 4
-const Proj = () => [projectPost, setProjectPost] = useState([{
-    
-   category: '',
-    title: '',
-    content: ''
-}])
-
-test('Ensure var take string', () =>{
-    expect(useState.category = "abcd");
-    expect(useState.title = "title");
-    expect(useState.content = "this is content");
-});
 
 //Test 5
 function filterData(inputs, key){
@@ -49,4 +16,5 @@ function filterData(inputs, key){
          expect(filterData.key = 'Technical Interview');
         
   });
-  
+
+ 
