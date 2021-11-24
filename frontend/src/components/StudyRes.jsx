b41d4e37962cd75b9fe17a9dd8248ed3002fb835
OjasweeP <91394698+OjasweeP@users.noreply.github.com>
2021-11-23 20:02:35 -0500

    Study Res/Tech Inter

--- frontend/src/components/StudyRes.jsx
+++ frontend/src/components/StudyRes.jsx
@@ -8,33 +8,22 @@ function showForm(){
     document.getElementById('show').innerHTML= document.getElementsById("postButton")
 
 
-}
+      }
+      
+      
 
-
-function StudyRes() {
-
-    return <div>
-       <div class="button col-auto">
-                <button type="button" id = "postButton" onClick = "showForm();" className="createStudResPost">Create Post</button>
-                </div>
-                
-                <form id= 'show'>
-                    <label>Create Study Resource Post</label>
-                    <input type = "text" name = "input" ></input>
-                    <br></br>
-                    <input type = "submit" value = "Post" name = "post"></input>
-                    <input type = "reset" value = "Reset" name = "reset"></input>
-                    <button type = "button" value = "Cancel" name= "cancButton"><a href = "./StudyRes"></a></button>
-                </form>
-            
+     const Posts = StudyResPost.map((p) => {
+          return <Post
+          title = {p.title}
+          content = {p.content}
+          />  
+      })
+    
               
                     
 
               
 
-        </div>
-           
-   
+                    </div>
 }
-
 export default StudyRes;
