56f382f5cca5365d05e71a2425cefe13c671954b
Bella-Su <bellabailey18@outlook.com>
2021-12-06 17:44:57 -0500

    add the comment button to post

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import './Post.css';
 import Comments from './Comments';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import FavoriteBorderRoundedIcon from '@material-ui/icons/FavoriteBorderRounded';
 
 function Post(props) {
     return (
@@ -10,6 +12,15 @@ function Post(props) {
                 <h6 className="card-subtitle mb-2 text-muted">{props.category}</h6>
                 <p className="card-text">{props.content}</p>
             </div>
+            <form className="comment">
+                    <button className="commentBt">
+                        <FavoriteBorderRoundedIcon /> likes
+                    </button>
+
+                    <button className="commentBt">
+                        <ChatBubbleOutlineIcon /> comments
+                    </button>
+                </form>
         </div>
     );
 
